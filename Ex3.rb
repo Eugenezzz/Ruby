f16378444c115da8b3c8ce269fffff80dfcdbde0
Eugenezzz <zaycevea@gmail.com>
2019-11-10 22:02:10 +0300

    Numbers and Math

---
+++ Ex3.rb
@@ -0,0 +1,57 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}" # 25 plus 30 slash 6
+puts "Roosters #{100 - 25 * 3 % 4}" # 100 minus 25 asterisk 3 percent of 4
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # 3 plus 2 plus 1 minus 5 plus 4 percent of 2 minus 1 slash 4 plus 6
+
+puts "Is it true that 3 + 2 < 5 - 7?" # False
+
+puts 3 + 2 < 5 - 7 # 3 plus 2 less-than 5 minus 7
+
+puts "What is 3 + 2? #{3 + 2}" # 3 plus 2
+puts "What is 5 - 7? #{5 - 7}" # 5 minus 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" # 5 greater-than -2
+puts "Is it greater or equal? #{5 >= -2}" # 5 greater-than-equal -2
+puts "Is it less or equal? #{5 <= -2}" # 5 less-than-equal -2
+
+
+
+
+
+puts "What is the number I like? #{100 % 3 * 10 + 2}" 
+
+
+
+
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25.0 + 30.0 / 6.0}" 
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}" 
+
+puts "Now I will count the eggs:"
+
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 
+
+puts "Is it true that 3 + 2 < 5 - 7?" 
+
+puts 3.0 + 2.0 < 5.0 - 7.0 
+
+puts "What is 3 + 2? #{3.0 + 2.0}" 
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5.0 > -2.0}" 
+puts "Is it greater or equal? #{5.0 >= -2.0}" 
+puts "Is it less or equal? #{5.0 <= -2.0}" 
