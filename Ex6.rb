8f845dc41181a8f5bed971b389ccbff01cbf5e41
Eugenezzz <zaycevea@gmail.com>
2019-11-10 22:37:08 +0300

    Strings and Text

---
+++ Ex6.rb
@@ -0,0 +1,47 @@
+# We create a variable
+types_of_people = 10
+# We put variable into a string
+x = "There are #{types_of_people} types of people."
+# We create a variable 
+binary = "binary"
+# We create a variable
+do_not = "don't"
+# We put variable into a string
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+# We put variable into a string
+puts "I said: #{x}."
+# We put variable into a string
+puts "I also said: '#{y}'."
+
+# We create a variable 
+hilarious = false
+# We put variable into a string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+# We create a variable
+w = "This is the left side of..."
+# We create a variable
+e = "a string with a right side."
+# We make a sum variables 
+puts w + e
+
+
+# Explain why adding the two strings w and e with + makes a longer string.
+# Because we sum two strings, so It will 2 times more text.
+
+
+# What happens when you change the strings to use ' (single-quote) instead of " (double-quote)? Do they still work? Try to guess why.
+# It works because they are different.
+
+
+
+
+
+
+
+
+
