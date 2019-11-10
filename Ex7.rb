19d3ceb8175141fae9b4cf2c8c9dbe1f92fcf620
Eugenezzz <zaycevea@gmail.com>
2019-11-10 22:39:24 +0300

    More Printing

---
+++ Ex7.rb
@@ -0,0 +1,27 @@
+
+puts "Mary had a little lamb."
+
+# We put a word into a string
+puts "Its fleece was white as #{'snow'}."
+
+puts "And everywhere that Mary went."
+# Here we multiply "." 10 times:
+puts "." * 10  # what'd that do?
+# Here we create a variables and names
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+# The option "print" display code in one line without wraps a string. That's why "puts" display code at the same line and only after this wrap a string.
+puts end7 + end8 + end9 + end10 + end11 + end12
